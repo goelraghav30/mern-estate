c3760de40519fb307f3bee6b71de512664f9ec67
Raghav Goel <raghavgoel577@gmail.com>
2024-06-05 16:27:35 +0530

    complete signup page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
-import Signout from "./pages/Signout";
+import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 
 export default function App() {
@@ -13,7 +13,7 @@ export default function App() {
   <Routes>
     <Route path="/" element={<Home />}/>
     <Route path="/signin" element={<Signin />}/>
-    <Route path="/signout" element={<Signout />}/>
+    <Route path="/signup" element={<SignUp />}/>
     <Route path="/profile" element={<Profile />}/>
     <Route path="/about" element={<About />}/>
   </Routes>
