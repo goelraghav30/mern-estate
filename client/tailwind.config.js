aa74a7aa8a125c0b095d51bac4a013d4111d34da
Raghav Goel <raghavgoel577@gmail.com>
2024-06-24 18:04:48 +0530

    removed plugin from tailwind file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,8 +7,10 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-    // ...
-  ],
+  plugins: 
+  // [
+
+  //   require('@tailwindcss/
+  //   // ...
+  // ],
 };
